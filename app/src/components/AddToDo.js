1e52d345ce4beda67844013ecc3783ecb86d0edb
Graham Theobald <Graham_Theobald@live.co.uk>
2022-11-13 21:25:53 +0000

    add button and box shadow

--- app/src/components/AddToDo.js
+++ app/src/components/AddToDo.js
@@ -4,10 +4,8 @@ import Check from './Check'
 import {v4 as uuid} from 'uuid'
 
 export default function AddToDo({handle}) {
-  
   const BLANK = {id: uuid(), text: '', complete: false}
   const [newTodo, setNewTodo] = useState(BLANK)
-
   return (
     <div className="add-todo">
       <Check 
@@ -25,6 +23,18 @@ export default function AddToDo({handle}) {
         type='text'
         value={newTodo.text} 
         placeholder='Create a todo...'/>
+      {
+        newTodo.text && 
+        <div 
+          onClick={() => {
+            if (!newTodo.text) return
+            handle(newTodo)
+            setNewTodo(BLANK)
+          }}
+          className="add">
+          Add
+        </div>
+      }
     </div>
   )
 }
