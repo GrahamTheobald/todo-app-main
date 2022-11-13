dafef451d136416a62676faa7393386cb16e7e9b
Graham Theobald <Graham_Theobald@live.co.uk>
2022-11-13 19:39:14 +0000

    theme: initial, storage and toggle

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -8,16 +8,29 @@ import data from '../data.json'
 import { v4 as uuid} from 'uuid'
 import '../css/app.css'
 
-function App() {
-  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+const STORAGE_KEY = 'todo-app-main'
 
-  const [darkTheme, setDarkTheme] = useState(defaultDark)
+function App() {
+  const [darkTheme, setDarkTheme] = useState()
   const [todos, setTodos] = useState(data)
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   useEffect(() => {
     window.addEventListener("resize", () => setWindowWidth(window.innerWidth))
+    initialTheme()
   }, [])
+
+  useEffect(() => {
+    if (darkTheme == undefined) return
+    localStorage.setItem(`${STORAGE_KEY}-theme`, darkTheme)
+  }, [darkTheme])
+ 
+  function initialTheme() {
+    const storageTheme = JSON.parse(localStorage.getItem(`${STORAGE_KEY}-theme`))
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+    setDarkTheme(storageTheme == undefined ? defaultDark : storageTheme)
+    console.log(darkTheme, storageTheme, defaultDark)
+  }
 
   const modeIcon = darkTheme ? dark : light
   const itemsLeft = todos.reduce((count, todo) => {
@@ -30,7 +43,10 @@ function App() {
       <div className='app'>
         <header className="header">
           <h1>Todo</h1>
-          <img src={modeIcon} alt="theme toggle"/>
+          <img 
+            onClick={() => setDarkTheme(!darkTheme)}
+            src={modeIcon} 
+            alt="theme toggle"/>
         </header>
         <AddToDo/>
         <div className="list">
